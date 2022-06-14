bd5367aadeb9c15e7cedd5b2d7b02121824910ed
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2022-06-14 10:32:16 +0600

    changed image format

--- index.js
+++ index.js
@@ -43,9 +43,7 @@ app.post("/api/ss", cors(), (req, res) => {
         args: ["--no-sandbox"],
       });
       const page = await browser.newPage();
-      await page.goto(req.body.site, {
-        waitUntil: "networkidle2",
-      });
+      await page.goto(req.body.site);
       setTimeout(async () => {
         await page.setViewport({
           width: req.body.width || 1920,
