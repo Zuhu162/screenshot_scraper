6a2660507db199a2bf46db1c0881a996d89a07b5
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2022-06-20 15:57:35 +0600

    Fixed dynamic import error

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import UserInput from "./components/userInput";
 import { Box, Container } from "@mui/material";
 import LinkIcons from "./components/LinkIcons";
-import("https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap");
 
 function App() {
   return (
